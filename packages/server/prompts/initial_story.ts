147788f7a41c6ad89ddd695bc4c19048edec356b
Corey Alexander <coreyja@gmail.com>
2023-10-29 14:21:55 -0400

    Get a start of a story from ChatGPT, now we need to vote

---
+++ packages/server/prompts/initial_story.ts
@@ -0,0 +1,27 @@
+export const initialStory = () =>
+  `
+  You are a spooky Halloyween themed story telling Chat Assitant. You come up with stories based on the user's input
+  The Stories are told in parts with user interaction in between.
+  Return a JSON object the follows the following schema:
+  {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+      "story": {
+        "type": "string"
+      },
+      "options": {
+        "type": "array",
+        "items": [
+          {
+            "type": "string"
+          }
+        ]
+      }
+    },
+    "required": [
+      "story",
+      "options"
+    ]
+  }
+`
--- packages/server/src/index.tsx
+++ packages/server/src/index.tsx
@@ -1,10 +1,69 @@
 import { Elysia, t } from "elysia";
 import { html } from "@elysiajs/html";
 import { staticPlugin } from "@elysiajs/static";
+import cron from "@elysiajs/cron";
+import OpenAI from "openai";
+import { initialStory } from "../prompts/initial_story";
+
+interface GameState {
+  currentStory: string;
+  options: string[];
+}
+
+const state: { game?: GameState } = { };
+
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_KEY"],
+});
+
+const parse = (content: string) => {
+  console.debug(content)
+  
+  const json = JSON.parse(content);
+
+  if (!json['story']) {
+    throw 'No story found'
+  }
+  if (!json['options']) {
+    throw 'No options found'
+  }
+
+  return json as { story: string, options: string[] }
+}
+
+const createNewStory = async () => {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: initialStory() }],
+    model: "gpt-3.5-turbo",
+  });
+
+  const content = chatCompletion.choices[0].message.content;
+
+  if (content) {
+    const parsedContent = parse(content);
+    state.game = {
+      currentStory: parsedContent.story,
+      options: parsedContent.options,
+    }
+  }
+
+  console.log(state);
+}
+
+createNewStory()
 
 const app = new Elysia()
   .use(html())
   .use(staticPlugin({ assets: "../client/out", prefix: "/" }))
+  .use(
+    cron({
+      name: "heartbeat",
+      pattern: "*/5 * * * *",
+      async run() {
+        await createNewStory()
+      },
+    }),
+  )
   .get("/", () => (
     <html lang="en">
       <head>
@@ -19,7 +78,7 @@ const app = new Elysia()
       </body>
     </html>
   ))
-  .get("/api/hello", () => ({ msg: "test" }))
+  .get("/api/game_state", () => state.game)
   .listen(3000);
 
 console.log(
