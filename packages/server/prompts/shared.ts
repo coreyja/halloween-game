6187a5c912ea75722a120a679ebcdcf44f3dcef7
Corey Alexander <coreyja@gmail.com>
2023-11-06 23:48:02 -0500

    New language

--- packages/server/prompts/shared.ts
+++ packages/server/prompts/shared.ts
@@ -1,4 +1,4 @@
 export const optionsDescription = () => `
 The Stories are told in parts with user interaction in between. There MUST be at least 3 options for the user to choose from.
-Make the options creative and spooky! No option should be to leave, we want the Users to be taking actions each turn not leaving the story
+Make the options creative and spooky! All the options should be actions for the User and should not involve ending the story
 `;
